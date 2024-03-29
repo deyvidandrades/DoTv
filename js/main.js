057d85a3fc30da3c79c638b128b42c2ea94ba3f9
deyvidandradees <deyvid-asilva@hotmail.com>
2024-03-29 11:18:08 -0300

    * Refatoração de main JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const fetchData = async (country_id = "BR") => {
 
-    loading(true, "Carregando canais.")
+    loading(true, "Carregando")
     try {
         const responsesJSON = await Promise.all([
             fetch('https://iptv-org.github.io/api/regions.json'),
@@ -36,40 +36,35 @@ const fetchData = async (country_id = "BR") => {
     }
 }
 
-let selected_country = 'BR'
-let channel_list = []
 let video_player = videojs('my-video', {
     controls: true,
     autoplay: false,
     log: false,
     sources: []
-});
-
-selected_country = localStorage.getItem("selected_country")
-if (selected_country === null) {
-    selected_country = "BR"
-    localStorage.setItem("selected_country", selected_country)
-    document.getElementById("pais_nome").sette
-}
-
-fetchData(selected_country).then(r => {
 })
 
-function loading(is_loading, titulo = "CARREGANDO INFORMAÇÕES", info = "") {
-    let loading = document.getElementById("loading")
-    document.getElementById("titulo_loading").innerText = titulo
-    document.getElementById("info_loading").innerText = info
+let raw_canais = []
+let canais_categoria = []
+let pais_selecionado_id = 'BR'
+pais_selecionado_id = localStorage.getItem("selected_country")
 
-    loading.style.visibility = is_loading ? "visible" : loading.style.visibility = "hidden"
+if (pais_selecionado_id === null) {
+    pais_selecionado_id = "BR"
+    localStorage.setItem("selected_country", pais_selecionado_id)
 }
 
 function init(regions, countries, categories, channels, streams) {
-    loadRegions()
-    loadCountries(regions, countries)
-    loadChannels2(channels, streams)
+    carregarRegioes()
+    carregarPaises(regions, countries)
+    raw_canais = filtrarCanais(channels, streams)
+    carregarCategorias(categories)
+    
+    loading(false)
 }
 
-function loadRegions() {
+fetchData(pais_selecionado_id).then()
+
+function carregarRegioes() {
     let regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
 
     let lista_regions = document.getElementById("tab-regions")
@@ -102,7 +97,7 @@ function loadRegions() {
     })
 }
 
-function loadCountries(regions, countries) {
+function carregarPaises(regions, countries) {
     regions.forEach(region => {
         if (['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'].includes(region["name"])) {
             countries.forEach(country => {
@@ -110,7 +105,7 @@ function loadCountries(regions, countries) {
                 if (region["countries"].includes(country["code"])) {
                     conteudo_country.innerHTML += `
                     <div class="col-6 col-md-4 col-lg-2 p-1 p-md-2">
-                    <div class="btn btn-light p-3 text-start w-100 h-100" onclick="changeCountry('${country["code"]}')">
+                    <div class="btn btn-light p-3 text-start w-100 h-100" onclick="mudarPais('${country["code"]}')">
                     <h4 class="my-auto me-2">${country["flag"]}</h4>
                     <p class="my-auto">${country["name"]}</hp>
                     </div>
@@ -118,7 +113,7 @@ function loadCountries(regions, countries) {
                     `
                 }
 
-                if (country["code"] === selected_country)
+                if (country["code"] === pais_selecionado_id)
                     document.getElementById("pais_nome").innerHTML = `
                     ${country["flag"]} ${country["name"].toUpperCase()}
                     `
@@ -127,10 +122,68 @@ function loadCountries(regions, countries) {
     })
 }
 
-function loadChannels2(channels, streams) {
+function carregarCategorias(categories) {
+    let lista_categorias = document.getElementById("tab-categorias")
+    let conteudo_categorias = document.getElementById("tabContent-categorias")
+
+    lista_categorias.innerHTML = ""
+    conteudo_categorias.innerHTML = ""
+    
+    let categorias = []
+    raw_canais.forEach(channel => {
+        if (!categorias.includes(channel["categories"][0])){
+            categorias.push(channel["categories"][0])
+        }
+    })
+    
+    categorias.sort()
+
+    categorias.forEach((category, i) => {
+        let lista_categorias_html = `
+            <li class="nav-item" role="presentation">
+                <button class="nav-link rounded-5 ${i === 0 ? "active" : ""}" id="tab-${category}" data-bs-toggle="pill"
+                    data-bs-target="#${category}" type="button" role="tab" aria-controls="${category}"
+                    aria-selected="true" onClick=testarCanaisCategoria("${category}")>
+                    ${category.toUpperCase()}
+                </button>
+            </li>
+        `
+        lista_categorias.innerHTML += lista_categorias_html
+    })
+
+    categorias.forEach((category, i) => {
+        let lista_conteudo_categorias_html = `
+            <div class="tab-pane fade ${i === 0 ? "show active" : ""}" id="${category}" role="tabpanel"
+                aria-labelledby="${category}-tab" tabindex="${i}">
+                <div class="row row-fluid" id="lista-canais-${category}"></div>
+            </div>`
+
+        conteudo_categorias.innerHTML += lista_conteudo_categorias_html
+    })
+}
+
+function carregarCanais(channels) {
+    
+    channels.forEach(channel => {
+        let conteudo_categorias = document.getElementById(`lista-canais-${channel["categories"][0]}`)
+
+        conteudo_categorias.innerHTML += `
+            <div class="col-12 col-md-4 col-lg-2 p-2">
+                <div class="btn btn-light px-3 py-4 text-start d-flex h-100" onclick="mudarCanal('${channel["id"]}')">
+                    <div class="col-3 my-auto py-auto">
+                        <img src="${channel["logo"]}" class="img img-fluid" alt="${channel["name"]}"/>
+                    </div>
+                    <p class="ms-3 my-auto col">${channel["name"]}</hp>
+                </div>
+            </div>
+        `
+    })
+}
+
+function filtrarCanais(channels, streams) {
     let filtered_channels = []
     channels.forEach(channel => {
-        if (channel["country"] === selected_country && channel["closed"] == null) {
+        if (channel["country"] === pais_selecionado_id && channel["closed"] == null) {
             streams.forEach(stream => {
                 if (stream["id"] === channel["id"]) {
                     filtered_channels.push({
@@ -145,157 +198,132 @@ function loadChannels2(channels, streams) {
             })
         }
     })
-
-    testChannels2(filtered_channels).then(working_channels => {
-        let lista_categorias = []
-
-        working_channels.forEach(channel => {
-            if (!lista_categorias.includes(channel["categories"][0]))
-                lista_categorias.push(channel["categories"][0])
-        })
-
-        loadCategories(lista_categorias)
-        exibir_lista_canais2(working_channels)
-        channel_list = working_channels
-        loading(false)
-    })
+    return filtered_channels
 }
 
-async function testChannels2(channels) {
-    let saved_channels = JSON.parse(localStorage.getItem('working_channels'))
-    let working_channels = []
-    let index = 0
+function loading(is_loading, titulo = "CARREGANDO INFORMAÇÕES", info = "") {
+    let loading = document.getElementById("loading")
+    document.getElementById("titulo_loading").innerText = titulo
+    document.getElementById("info_loading").innerText = info
+
+    loading.style.visibility = is_loading ? "visible" : loading.style.visibility = "hidden"
+}
+
+async function testarCanais(canais){
+    let canais_funcionando = []
+    let canais_salvos = {}
+    let index = 1
+
+    if(localStorage.getItem('working_channels'))
+        canais_salvos = JSON.parse(localStorage.getItem('working_channels'))
+    
+    if(canais_salvos === null || canais_salvos[pais_selecionado_id] === undefined)
+        canais_salvos[pais_selecionado_id] = []
 
     loading(true, "Testando canais", ``)
-    if (saved_channels == null || saved_channels[selected_country] === undefined) {
-        for (const channel of channels) {
-            await checkVideoSource(channel["url"]).then(is_working => {
-                working_channels.push(channel["id"])
+    
+    for (const channel of canais) {
+        if(!canais_salvos[pais_selecionado_id].includes(channel["id"])){
+            await verificarVideoSource(channel["url"]).then(is_working => {
+                canais_funcionando.push(channel["id"])
+                
             }).catch(error => {
-
+                
             })
-
-            loading(
-                true,
-                "Testando canais",
-                `(${index}/${channels.length}) Canal: ${channel["name"]}`
-            )
-
-            index += 1
         }
-
-        saved_channels[selected_country] = working_channels
-
-        localStorage.setItem('working_channels', JSON.stringify(saved_channels))
+        
+        loading(
+            true,
+            `Testando canais (${channel["categories"]})`,
+            `(${index}/${canais.length}) Canal: ${channel["name"]}`
+        )
+        
+        index += 1
     }
-
-    let new_list = []
-    channels.forEach(channel => {
-        if (saved_channels[selected_country].includes(channel["id"]))
-            new_list.push(channel)
+    video_player.reset()
+    
+    canais_funcionando.forEach(canal => {
+        if(!canais_salvos[pais_selecionado_id].includes(canal))
+            canais_salvos[pais_selecionado_id].push(canal)
     })
 
-    return new_list
+    localStorage.setItem('working_channels', JSON.stringify(canais_salvos))
+    
+    loading(false)
+    
+    return canais_salvos[pais_selecionado_id]
 }
 
-function loadCategories(categories) {
-    categories.sort()
-
-    let lista_categorias = document.getElementById("tab-categorias")
-    let conteudo_categorias = document.getElementById("tabContent-categorias")
-
-    lista_categorias.innerHTML = ""
-    conteudo_categorias.innerHTML = ""
-
-    categories.forEach((category, i) => {
-        let lista_categorias_html = `
-            <li class="nav-item" role="presentation">
-                <button class="nav-link rounded-5 ${i === 0 ? "active" : ""}" id="tab-${category}" data-bs-toggle="pill"
-                    data-bs-target="#${category}" type="button" role="tab" aria-controls="${category}"
-                    aria-selected="true">
-                    ${category.toUpperCase()}
-                </button>
-            </li>
-        `
-        lista_categorias.innerHTML += lista_categorias_html
+function testarCanaisCategoria(categoria) {
+    let lista_canais_categoria = []
+    
+    raw_canais.forEach(channel => {
+        if (channel["categories"].includes(categoria))
+            lista_canais_categoria.push(channel)
     })
 
-    categories.forEach((category, i) => {
-        let lista_conteudo_categorias_html = `
-            <div class="tab-pane fade ${i === 0 ? "show active" : ""}" id="${category}" role="tabpanel"
-                aria-labelledby="${category}-tab" tabindex="${i}">
-                <div class="row row-fluid" id="lista-canais-${category}"></div>
-            </div>`
+    testarCanais(lista_canais_categoria).then(canais_funcionando => {
+        let lista_canais = []
+        
+        lista_canais_categoria.forEach(canal => {
+            if(canais_funcionando.includes(canal["id"])){
+                lista_canais.push(canal)
+                document.getElementById(`lista-canais-${canal["categories"][0]}`).innerHTML = ''
+            }
+        })
 
-        conteudo_categorias.innerHTML += lista_conteudo_categorias_html
+        carregarCanais(lista_canais)
+        canais_categoria = lista_canais
     })
 }
 
-function exibir_lista_canais2(channels) {
-    channels.forEach(channel => {
-        let conteudo_categorias = document.getElementById(`lista-canais-${channel["categories"][0]}`)
-
-        conteudo_categorias.innerHTML += `
-            <div class="col-12 col-md-4 col-lg-2 p-2">
-                <div class="btn btn-light px-3 py-4 text-start d-flex h-100" onclick="changeChannel('${channel["id"]}')">
-                    <div class="col-3 my-auto py-auto">
-                        <img src="${channel["logo"]}" class="img img-fluid" alt="${channel["name"]}"/>
-                    </div>
-                    <p class="ms-3 my-auto col">${channel["name"]}</hp>
-                </div>
-            </div>
-        `
-    })
+function mudarPais(country_id) {
+    pais_selecionado_id = country_id
+    localStorage.setItem("selected_country", pais_selecionado_id)
+    fetchData(country_id).then()
 }
 
-function changeCountry(country_id) {
-    selected_country = country_id
-    localStorage.setItem("selected_country", selected_country)
-    fetchData(country_id).then(r => {
-    })
-}
-
-function changeChannel(channel_id) {
+function mudarCanal(channel_id) {
     let player = document.getElementById("my-video")
     let channel_name = document.getElementById("canal_nome")
     let channel_category = document.getElementById("canal_categoria")
     let channel_capa = document.getElementById("canal_capa")
     let new_channel = {}
 
-    channel_list.forEach(channel => {
+    canais_categoria.forEach(channel => {
         if (channel["id"] === channel_id) {
             channel_name.innerText = channel["name"]
             channel_category.innerText = `Categoria: ${channel["categories"]}`
             channel_capa.src = channel["logo"]
 
-            loadChannel(channel)
+            video_player.src({src: channel["url"], type: 'application/x-mpegURL'})
+            video_player.poster(channel["logo"])
+
+            //video_player.on('error', player_error_listener(channel))
+            video_player.load()
+            video_player.play()
+
+            window.scrollTo({top: 0, behavior: 'smooth'})
         }
     })
 }
 
-function loadChannel(channel) {
-    video_player.src({src: channel["url"], type: 'application/x-mpegURL'})
-    video_player.poster(channel["logo"])
-
-    //video_player.on('error', player_error_listener(channel))
-    video_player.load()
-    video_player.play()
-
-    window.scrollTo({top: 0, behavior: 'smooth'})
-}
-
-function checkVideoSource(url) {
+function verificarVideoSource(url) {
     return new Promise((resolve, reject) => {
         let loadTimeout = setTimeout(() => {
-            video_player.off('loadedmetadata', loadedMetadataHandler);
-            video_player.off('error', errorHandler);
-            reject(false);
-        }, 10000)
+            video_player.off('loadedmetadata', loadedMetadataHandler)
+            video_player.off('error', errorHandler)
+
+            video_player.pause()
+            reject(true)
+        }, 20000)
 
         const loadedMetadataHandler = () => {
             clearTimeout(loadTimeout)
             video_player.off('loadedmetadata', loadedMetadataHandler)
             video_player.off('error', errorHandler)
+
+            video_player.pause()
             resolve(true)
         }
 
@@ -303,8 +331,9 @@ function checkVideoSource(url) {
             clearTimeout(loadTimeout)
             video_player.off('loadedmetadata', loadedMetadataHandler)
             video_player.off('error', errorHandler)
-
-            reject(false)
+            
+            video_player.pause()
+            reject(true)
         }
 
         video_player.on('error', errorHandler)
